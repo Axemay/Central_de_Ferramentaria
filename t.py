f3357802316fe547d5b0d59e615a02c3fa485f81
Daniel Nepomuceno <105947140+dpsndroid@users.noreply.github.com>
2022-08-30 11:52:56 -0300

    construindo novos frames e nova composição

--- t.py
+++ t.py
@@ -115,7 +115,24 @@ def janela_cadastro_tecnicos():
     cadastro_tecnicos.resizable(True, True)
     cadastro_tecnicos.maxsize(width= 900, height=600) # dimensões máximas
     cadastro_tecnicos.minsize(width= 400, height= 300) # dimensões mínimas
-    
+
+############################   FRAMES   ######################################
+
+    frame1 = Frame(cadastro_tecnicos)
+    label1 = tk.Label(frame1, text="Label 1")
+    frame1.place(relx= 0.00, rely=0.00, relwidth=0.75, relheight=0.35)
+
+    frame2 = Frame(cadastro_tecnicos)
+    label2 = tk.Label(frame2, text="Label 2")
+    frame2.place(relx= 0.76, rely=0.00, relwidth=0.26, relheight=0.35)
+
+    frame3 = Frame(cadastro_tecnicos)
+    label3 = tk.Label(frame3, text="Label 3")
+    frame3.place(relx= 0.00, rely=0.36, relwidth=1, relheight=0.46)
+
+
+##############################################################################  
+
     cpf = tk.Label(cadastro_tecnicos, text="CPF:", bg='#ffd')
     cpf.place(relx=0.05, rely=0.02, relwidth=0.1, relheight=0.05)
     vcpf = tk.Entry(cadastro_tecnicos)
@@ -141,19 +158,22 @@ def janela_cadastro_tecnicos():
     vequipe = tk.Entry(cadastro_tecnicos)
     vequipe.place(relx= 0.2, rely= 0.26, relwidth=0.5, relheight=0.05)
     
-    bsalvar = tk.Button(cadastro_tecnicos, text='Salvar Cadastro', command= codigo_tecnicos)
-    bsalvar.place(relx = 0.24, rely= 0.35, relwidth=0.2, relheight= 0.05)
+    bsalvar = tk.Button(frame2, text='Salvar Cadastro', command= codigo_tecnicos)
+    bsalvar.place(relx = 0.05, rely= 0.20, relwidth=0.8, relheight= 0.15)
 
-    blimpar = tk.Button(cadastro_tecnicos, text="Limpar Campos", command=limpa_tela)
-    blimpar.place(relx=0.46, rely=0.35, relwidth=0.2, relheight=0.05)
+    blimpar = tk.Button(frame2, text="Limpar Campos", command=limpa_tela)
+    blimpar.place(relx=0.05, rely=0.40, relwidth=0.8, relheight=0.15)
 
-    res = tk.Label(cadastro_tecnicos, text="Insira acima os dados do funcionário", bg="#B9B7BD",font= ("verdana",11))
-    res.place(relx= 0.05, rely= 0.50, relwidth = 0.8, relheight=0.05)
+    res = tk.Label(frame3, text="Insira acima os dados do funcionário", bg="#B9B7BD",font= ("verdana",11))
+    res.place(relx= 0.05, rely= 0.10, relwidth = 0.9, relheight=0.05)
 
-    op = tk.Label(cadastro_tecnicos, text="TURNO: Manhã (m), Tarde (t), Noite (n)\n\nCPF deve conter exatamente 11 dígitos\n\nTelefone deve conter 10 dígitos com prefixo incluso\n\nTodos os campos devem ser preenchidos", bg="#B9B7BD",font= ("verdana",9))
-    op.place(relx= 0.05, rely= 0.60, relwidth = 0.8, relheight=0.40)
-   
-    
+    op = tk.Label(frame3, text="TURNO: Manhã (m), Tarde (t), Noite (n)\n\nCPF: 11 dígitos\n\nTelefone fixo: 10 dígitos, Celular: 11 dígitos", bg="#B9B7BD",font= ("verdana",9))
+    op.place(relx= 0.05, rely= 0.25, relwidth = 0.9, relheight=0.45)
+
+
+########################   CONSULTA TÉCNICOS     ####################
+
+
 def janela_consulta_tecnicos():
     consulta_tecnicos = tk.Toplevel()
     consulta_tecnicos.title('Janela de Consulta de Técnicos')
@@ -167,12 +187,12 @@ def janela_consulta_tecnicos():
     #combobox.place(relx = 0.02, rely= 0.15, relwidth=0.75, relheight=0.05)
 
     
-    frame1 = ttk.Frame(consulta_tecnicos, width=600, height=300)
+    frame2 = ttk.Frame(consulta_tecnicos, width=600, height=300)
 
-    hscrollbar = ttk.Scrollbar(frame1, orient=tk.HORIZONTAL)
-    vscrollbar = ttk.Scrollbar(frame1, orient=tk.VERTICAL)
-    sizegrip = ttk.Sizegrip(frame1)
-    canvas = tk.Canvas(frame1, bd=0, highlightthickness=0, yscrollcommand=vscrollbar.set, xscrollcommand=hscrollbar.set)
+    hscrollbar = ttk.Scrollbar(frame2, orient=tk.HORIZONTAL)
+    vscrollbar = ttk.Scrollbar(frame2, orient=tk.VERTICAL)
+    sizegrip = ttk.Sizegrip(frame2)
+    canvas = tk.Canvas(frame2, bd=0, highlightthickness=0, yscrollcommand=vscrollbar.set, xscrollcommand=hscrollbar.set)
     vscrollbar.config(command=canvas.yview)
     hscrollbar.config(command=canvas.xview)
     
@@ -194,7 +214,7 @@ def janela_consulta_tecnicos():
     vscrollbar.pack(fill=tk.Y, side=tk.RIGHT, expand=tk.FALSE)
     sizegrip.pack(in_=hscrollbar, side=tk.BOTTOM, anchor="se")
     canvas.pack(side=tk.LEFT, padx=5, pady=5, fill=tk.BOTH, expand=tk.TRUE)
-    frame1.pack(padx=5, pady=5, expand=True, fill=tk.BOTH)
+    frame2.pack(padx=5, pady=5, expand=True, fill=tk.BOTH)
 
     canvas.create_window(0, 0, window=subframe)
     consulta_tecnicos.update_idletasks()
