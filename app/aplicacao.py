57f42987eb25de941b4488749eb5d7406855daf9
axemay <maiara.accacio@gmail.com>
2022-09-08 10:04:44 -0300

    atualizacao sobre

--- app/aplicacao.py
+++ app/aplicacao.py
@@ -11,7 +11,7 @@ import webbrowser
 janela = tk.Tk()
 
 
-class Aplicacao(TK, front_Ferramentas, Reserva):
+class Aplicacao(TK, front_Ferramentas, Reserva, sobre):
     def __init__(self):
         TK.__init__(self)
         front_Ferramentas.__init__(self)
@@ -77,11 +77,13 @@ class Aplicacao(TK, front_Ferramentas, Reserva):
         self.bt_cadastrar_reserva = tk.Button(self.frame_reserva, text="Acessar", bd=3, font=('poppins', 19, 'bold'), command= self.janela_cadastro_reservas)
         self.bt_cadastrar_reserva.place(relx=0.1, rely=0.8, relwidth=0.8, relheight=0.15)
 
+        self.bt_sobre = tk.Button(self.janela, text="Sobre", bd=5, command=self.janela_sobre)
+        self.bt_sobre.place(relx=0.68, rely=0.90, relwidth=0.13, relheight=0.05)
+
         self.bt_manual = tk.Button(self.janela, text="Consultar Manual", bd=5, command=self.abrir_manual)
         self.bt_manual.place(relx=0.82, rely=0.90, relwidth=0.13, relheight=0.05)
 
-        self.bt_sobre = tk.Button(self.janela, text="Sobre", bd=5, command=janela_sobre)
-        self.bt_sobre.place(relx=0.68, rely=0.90, relwidth=0.13, relheight=0.05)
+
 
     def labels(self):
         self.label_titulo = tk.Label(self.frame_titulo, text="Central de Ferramentaria", bg='#B9B7BD', font=('poppins', 50, 'bold'))
--- app/sobre.py
+++ app/sobre.py
@@ -1,18 +1,37 @@
 import tkinter as tk
+from app.tecnico import TK
 
-
-def janela_sobre():
+# janela_sobre =  tk.Tk()
+class sobre():
+    def janela_sobre(self):
             
-            sobre = tk.Tk()
-            sobre.title('Sobre')
-            sobre.configure(background='#B9B7BD')
-            sobre.geometry('800x700')
-            sobre.resizable(True, True)
-            sobre.minsize(width=600, height=400)
-            
+            self.sobre = tk.Toplevel()
+            self.sobre.title('Sobre')
+            self.sobre.iconphoto(False, tk.PhotoImage(file='../ico/info.png'))
+            self.sobre.configure(background='#B9B7BD')
+            self.sobre.geometry('800x700')
+            self.sobre.resizable(True, True)
+            self.sobre.minsize(width=600, height=400)
 
-            bt_fechar = tk.Button(sobre, text="Encerrar Manual", bd=5, command=sobre.destroy)
-            bt_fechar.place(relx=0.40, rely=0.93, relwidth=0.20, relheight=0.05)
+            self.frame_sobre = tk.Frame(self.sobre, bd=4, bg='#fff',
+                                         highlightbackground='#B9B7BD',
+                                         highlightthickness=2)
+            self.frame_sobre.place(relx=0.05, rely=0, relwidth=0.91, relheight=0.3)
+
+            self.logo_estacio = tk.PhotoImage(file='../ico/logo1.png')
+            self.label_logo = tk.Label(self.frame_sobre, image=self.logo_estacio)
+            self.label_logo.place(relx=0.3, rely=0.1, relwidth=0.385, relheight=0.35)
+            
+            # label_estacio = tk.Label(frame_sobre, text="Universidade Estácio de Sá", bg='#fff', font=('poppins', 25, 'bold'))
+            # logo.estacio = tk.PhotoImage(file='../ico/worker.png')
+            # label_tec_Img = tk.Label(self.frame_tecnico, image=self.img_tec)
+            # label_tec.place(relx=0.3, rely=0.5, relwidth=0.4, relheight=0.2)
+            # label_tec_Img.place(relx=0.29, rely=0.05, relwidth=0.4, relheight=0.4)
+
+
+
+            self.bt_fechar = tk.Button(self.sobre, text="Encerrar", bd=5, command=self.sobre.destroy)
+            self.bt_fechar.place(relx=0.40, rely=0.93, relwidth=0.20, relheight=0.05)
 
             
 
